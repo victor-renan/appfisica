46b8903ff2a6faf040d1a62e6c8ecceb0210ba1a
victor-renan <alvesrenanpro@gmail.com>
2023-05-11 22:22:45 -0300

    fix(): pacotes desnecessarios excluidos

--- App.jsx
+++ App.jsx
@@ -2,19 +2,19 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { NavigationDrawer } from './components/Navigation';
-import { NativeBaseProvider, Box} from "native-base";
+import { NativeBaseProvider, View } from "native-base";
 
 
 // Exporta a montagem do App
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box flex={1}>
+      <View flex={1}>
         <StatusBar />
         <NavigationContainer>
           <NavigationDrawer />
         </NavigationContainer>
-      </Box>
+      </View>
     </NativeBaseProvider>
   );
 }
